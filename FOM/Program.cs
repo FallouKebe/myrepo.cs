3818343a748c54240011d6d9f2b75195740d6869
Fallou Kebe <falloukebe0710@gmail.com>
2024-12-19 16:45:15 +0000

    Exercice Assurance

---
+++ FOM/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Programme
+{
+    static void Main()
+    {
+        // Demande à l'utilisateur de saisir le montant des dommages
+        Console.Write("Entrez le montant des dommages: ");
+        double montantDommages = Convert.ToDouble(Console.ReadLine());
+
+        // Calcul de la franchise (10% du montant des dommages, mais limitée à 4000 cfa)
+        double franchise = montantDommages * 0.10;
+        if (franchise > 4000)
+        {
+            franchise = 4000;
+        }
+
+        double montantRembourse = montantDommages - franchise;
+
+        Console.WriteLine("Montant remboursé: " + montantRembourse + " cfa");
+        Console.WriteLine("Franchise: " + franchise + " cfa");
+    }
+}
