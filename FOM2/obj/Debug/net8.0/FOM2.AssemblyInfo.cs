dcded70ecce05d3b54ceb80d639c7b054c1aad13
Fallou Kebe <falloukebe0710@gmail.com>
2024-12-19 17:03:21 +0000

    fUSION

--- FOM2/obj/Debug/net8.0/FOM2.AssemblyInfo.cs
+++ FOM2/obj/Debug/net8.0/FOM2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FOM2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f57672b20cc71b7336221995c154eabd4c942658")]
 [assembly: System.Reflection.AssemblyProductAttribute("FOM2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FOM2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
