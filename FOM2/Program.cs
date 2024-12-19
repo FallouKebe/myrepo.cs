f57672b20cc71b7336221995c154eabd4c942658
Fallou Kebe <falloukebe0710@gmail.com>
2024-12-19 16:57:10 +0000

    Exercice 2 factorielle

---
+++ FOM2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Programme
+{
+    static void Main()
+    {
+        // Demande à l'utilisateur de saisir un nombre
+        Console.Write("Entrez un nombre pour calculer sa factorielle: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        // Calcul de la factorielle
+        long factorielle = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            factorielle *= i;
+        }
+
+        // Affichage du résultat
+        Console.WriteLine("La factorielle de " + n + " est: " + factorielle);
+    }
+}
